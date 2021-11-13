90e3bdfe67517195030efcb24c932986b9c2ce46
Roger Pease <rogerpease@gmail.com>
2021-11-12 20:14:44 -0600

    Added rm of prior build.

--- RunVerilatorStreamElement.sh
+++ RunVerilatorStreamElement.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd Verilator/StreamElement
+rm obj_dir/TestStreamElement
 verilator --cc --exe --trace --build -j TestStreamElement.cpp ../../Verilog/module/StreamElement.sv  -o TestStreamElement
 ./obj_dir/TestStreamElement
