5284103b215fdcfb0e35c8da3b053cca2c7dff2d
Roger Pease <rogerpease@gmail.com>
2022-01-09 08:05:11 -0600

    Initial Revision

--- scripts/synthesis.module.tcl
+++ scripts/synthesis.module.tcl
@@ -1,7 +1,7 @@
 read_verilog [ glob ./Verilog/module/*.sv ./Verilog/module/*.v ]
 # Run synthesis
 synth_design -top AXIStreamCompressor  -part xc7z020clg400-1 -flatten_hierarchy rebuilt
-create_clock -period 10.000 -name sys_clk_100mhz -waveform {0.000 5.000} [get_ports clk]
+create_clock -period 10.000 -name sys_clk_100mhz -waveform {0.000 5.000} [get_ports *clk]
 # Write design checkpoint
 write_checkpoint -force synthesis/module/post_synth
 # Write report utilization and timing estimates
